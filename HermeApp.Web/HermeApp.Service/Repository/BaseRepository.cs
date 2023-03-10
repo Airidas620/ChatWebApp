d08f6674cab35d23a0ac472f8a14e72b79e01abb
Imran Haider <imranhaider.se@gmail.com>
2023-03-10 15:39:37 +0500

    Created Repository for CRUD operations

---
+++ HermeApp.Web/HermeApp.Service/Repository/BaseRepository.cs
@@ -0,0 +1,49 @@
+﻿using HermeApp.Service.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HermeApp
+{
+    public abstract class BaseRepository<T> where T : class
+    {
+        protected readonly HermeAppWebContext dbContext;
+
+        protected BaseRepository(HermeAppWebContext context)
+        {
+            dbContext = context;
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await dbContext.Set<T>().FindAsync(id);
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task CreateAsync(T entity)
+        {
+            await dbContext.Set<T>().AddAsync(entity);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            dbContext.Set<T>().Update(entity);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            dbContext.Set<T>().Remove(entity);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+
+}
---
+++ HermeApp.Web/HermeApp.Service/Repository/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using HermeApp.Service.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HermeApp
+{
+    public class UserRepository:BaseRepository<HermeAppWebUser>
+    {
+        public UserRepository(HermeAppWebContext context) : base(context)
+        {
+        }
+    }
+}
